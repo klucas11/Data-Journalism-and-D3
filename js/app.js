1e42cd141bf0f6609cfd8a5633bce3514947b853
Kristin Lucas <klucas11@aggienetwork.com>
2019-09-11 23:19:02 -0500

    Creates data points and axis labels

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // Creates margins for image
-var svgWidth = 960;
+var svgWidth = 800;
 var svgHeight = 500;
 
 var margin = {
@@ -25,15 +25,15 @@ var chartGroup = svg.append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 // Import data from the CSV file
-d3.csv("data/data.csv", function (error, healthData) {
-    if (error) throw error;
+d3.csv("data/data.csv").then(function (healthData) {
 
     // Format the data
     healthData.forEach(function (data) {
         data.poverty = +data.poverty;
         data.evening = +data.healthcare;
     });
-    // Create the scales for the chart
+
+    // Create the scales
     var xScale = d3.scaleLinear()
         .domain(d3.extent(healthData, d => d.poverty))
         .range([0, width]);
@@ -42,7 +42,7 @@ d3.csv("data/data.csv", function (error, healthData) {
         .domain(d3.extent(healthData, d => d.healthcare))
         .range([height, 0]);
 
-    // Create the axes
+    // Create the axis
     var bottomAxis = d3.axisBottom(xScale);
     var leftAxis = d3.axisLeft(yScale);
 
@@ -54,3 +54,40 @@ d3.csv("data/data.csv", function (error, healthData) {
     // Add y-axis
     chartGroup.append("g").call(leftAxis);
 
+    // Create circles
+    chartGroup.selectAll("circle")
+        .data(healthData)
+        .enter()
+        .append("circle")
+        .attr("cx", d => xScale(d.poverty))
+        .attr("cy", d => yScale(d.healthcare))
+        .attr("r", 10)
+        .attr("fill", "blue")
+        .attr("opacity", ".5");
+
+    // Label circles by state
+    chartGroup.selectAll()
+        .data(healthData)
+        .enter()
+        .append('text')
+        .attr("x", d => xScale(d.poverty) - 5)
+        .attr("y", d => yScale(d.healthcare) + 3)
+        .attr("fill", "white")
+        .attr("font-size", "8")
+        .text(d => d.abbr);
+
+    // Create X axis label
+    chartGroup.append('text')
+        .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+        .attr("class", "axisText")
+        .text("In Poverty (%)");
+
+    // Create Y axis label
+    chartGroup.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr('y', 0 - margin.left + 50)
+        .attr("x", 0 - (height / 2))
+        .attr("class", "axisText")
+        .text("Lacks Healthcare (%)");
+
+});
